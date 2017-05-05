f76e946c9fa1a12bff65f37c80a7c1796bc68e97
Aaron Klump <sourcecode@intheloftstudios.com>
2017-05-04 17:02:27 -0700

    add hook_release_info add is_live and loft_test_class

--- loft_core.api.php
+++ loft_core.api.php
@@ -18,10 +18,57 @@
  *                            MENU_ACCESS_DENIED or MENU_NOT_FOUND if you wish
  *                            and the appropriate action will be taken.
  */
-function HOOK_loft_core_redirect_node_BUNDLE_TYPE($node)
+function HOOK_loft_core_redirect_node_BUNDLE_TYPE($node, $op)
 {
     return [
         'node/' . BLOG_PAGE_NID,
         ['fragment' => 'm[]=modal,blog_entry__' . $node->nid],
+    ];
+}
+
+/**
+ * Implements hook_loft_core_code_release_info().
+ *
+ * @return array
+ *   Each element key is a unique feature key/tag.  Each element is an array with:
+ *     - is_live bool False to disable the feature.
+ */
+function hook_loft_core_code_release_info()
+{
+    return [
+        'photoshare' => [
+            'is_ready' => false,
+            'is_live' => false,
+            'description' => 'Ability to share individual photo essay photos.',
+        ],
+        'passhelp' => [
+            'is_ready' => false,
+            'is_live' => false,
+            'description' => 'Link during login proess to open a modal where user can request a new password',
+        ],
+        'facebook' => [
+            'is_ready' => false,
+            'is_live' => false,
+        ],
+        'comments' => [
+            'is_ready' => false,
+            'is_live' => false,
+        ],
+        'avatars' => [
+            'is_ready' => false,
+            'is_live' => false,
+        ],
+        'blog' => [
+            'is_ready' => false,
+            'is_live' => false,
+        ],
+        'wysiwyg' => [
+            'is_ready' => false,
+            'is_live' => false,
+        ],
+        'tour' => [
+            'is_ready' => true,
+            'is_live' => false,
+        ],
     ];
 }
