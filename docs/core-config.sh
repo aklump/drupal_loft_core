9502a8dae5179e49aa3f953e669cf3948ea634da
Aaron Klump <sourcecode@intheloftstudios.com>
2017-09-11 17:10:52 -0700

    update docs

--- docs/core-config.sh
+++ docs/core-config.sh
@@ -4,57 +4,75 @@
 # Configuration
 
 ##
- # An array of output formats to disable, if any
- #
-#disabled = "website drupal html text mediawiki doxygene"
-disabled = "html text mediawiki doxygene"
+# An array of output formats to disable, if any
+#
+#disabled = "website drupal html text mediawiki"
+#disabled = "drupal html text mediawiki"
 
 ##
- # File path to the php you want to use for compiling
- #
+# File path to the php you want to use for compiling
+#
 php = $(which php)
 #php = '/Applications/MAMP/bin/php/php5.3.14/bin/php'
 
 ##
- # Lynx is required for output of .txt files
- #
+# Lynx is required for output of .txt files
+#
 lynx = $(which lynx)
 
 ##
- # The drupal credentials for a user who can access your iframe content
- #
+# The drupal credentials for a user who can access your iframe content
+#
 #credentials = "http://user:pass@www.my-site.com/user/login";
 
 ##
- # The name of the drupal module to build advanced help output for, if
- # applicable
- #
-drupal_module = 'loft_core';
+# The name of the drupal module to build advanced help output for, if
+# applicable
+#
+#drupal_module = 'my_module';
 
 ##
- # The location of the advanced help output; this location is used in place of
- # the default, if enabled.
- #
-drupal_dir = '../help'
+# The location of the advanced help output; this location is used in place of
+# the default, if enabled.  It is relative to the directory containing core-config.sh.
+#
+#drupal_dir = '../help'
 
 ##
- # The file path to an extra README.txt file; when README.md is compiled and
+# The file path to an extra README.txt file; when README.md is compiled and
+# this variable is set, the .txt version will be copied to this location.  Notice the second path will also copy the md version.  If this is not desired then omit the second path
+#
+# This MUST be a directory relative to the directory containing core-config.sh
+#
+#README = '../README.txt ../README.md'
+
+##
+ # The file path to an extra CHANGELOG.txt file; when CHANGELOG.md is compiled and
  # this variable is set, the .txt version will be copied to this location.
- # 
- # This should be a relative directory, relative to the source directory.
  #
-#README = '../../README.txt'
+#CHANGELOG = '../CHANGELOG.txt ../CHANGELOG.md'
 
-# This would also copy README.md as well as README.txt to the directory one
-# level above /source
-README = '../README.txt ../README.md'
-
-#root_dir      = ""
+#root_dir = ""
 
 #
-# Defines pre/post hooks, shell or php scripts to call, space separated.
-# 
+# Defines pre/post hooks, shell or php scripts to call, space separated.  These must be placed in a directory called 'hooks' one level above source.
+#
 #pre_hooks = "pre_compile.sh pre_compile.php"
 #post_hooks = "post_compile.sh post_compile.php"
 
-version_file = "../loft_core.info"
+#
+# The path to a .info file or a .json file containing 'version' as a first level key, whose value indicates the documentation version.
+# This can be relative to the directory containing core-config.sh or absolute if it begins with a /
+#version_file = "web_package.info"
+
+#
+# These paths are relative to the directory containing core-config.sh.
+#website_dir = 'public_html'
+#html_dir = 'html'
+#mediawiki_dir = 'mediawiki'
+#text_dir = 'text'
+#drupal_dir = 'advanced_help'
+
+#
+# This controls which file extensions are run through the markdown parser and wrapped with the header/footer tpls.
+#partial_extension = '.md'
+
