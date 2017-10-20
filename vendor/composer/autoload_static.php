bf7648efcede1e3bb52a5be467e78f0a0a7c1981
Aaron Klump <sourcecode@intheloftstudios.com>
2017-10-19 17:39:08 -0700

    added php-simple-html-parser

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -28,61 +28,14 @@ class ComposerStaticInita53fe36df132dd600a8787a349a9d1f5
         ),
     );
 
-    public static $classMap = array (
-        'AKlump\\LoftLib\\Code\\Arrays' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Code/Arrays.php',
-        'AKlump\\LoftLib\\Code\\Exposer' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Code/Exposer.php',
-        'AKlump\\LoftLib\\Code\\Grammar' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Code/Grammar.php',
-        'AKlump\\LoftLib\\Code\\PhpDocBlock' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Code/PhpDocBlock.php',
-        'AKlump\\LoftLib\\Code\\String' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Code/String.php',
-        'AKlump\\LoftLib\\Code\\Strings' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Code/Strings.php',
-        'AKlump\\LoftLib\\Component\\Bash\\Bash' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Bash/Bash.php',
-        'AKlump\\LoftLib\\Component\\Config\\Config' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/Config.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigBash' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigBash.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigCookieJar' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigCookieJar.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigDrupalInfo' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigDrupalInfo.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigFileBasedStorage' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigFileBasedStorage.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigIni' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigIni.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigInterface' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigInterface.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigJson' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigJson.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigPdfMetadata' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigPdfMetadata.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigYaml' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigYaml.php',
-        'AKlump\\LoftLib\\Component\\Config\\ConfigYamlFrontMatter' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Config/ConfigYamlFrontMatter.php',
-        'AKlump\\LoftLib\\Component\\Forms\\BasicForm' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Forms/BasicForm.php',
-        'AKlump\\LoftLib\\Component\\Forms\\InvalidFormException' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Forms/InvalidFormException.php',
-        'AKlump\\LoftLib\\Component\\Forms\\InvalidFormItemException' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Forms/InvalidFormItemException.php',
-        'AKlump\\LoftLib\\Component\\Forms\\InvalidFormItemValueException' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Forms/InvalidFormItemValueException.php',
-        'AKlump\\LoftLib\\Component\\Forms\\MissingRequiredFormItemException' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Forms/MissingRequiredFormItemException.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\Drupal7\\EntityPdfInterface' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/Drupal7/EntityPdfInterface.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\Drupal7\\LibreOfficeConverter' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/Drupal7/LibreOfficeConverter.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\Drupal7\\PdfArchiveEntityPdf' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/Drupal7/PdfArchiveEntityPdf.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\Drupal7\\PdfTkMerger' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/Drupal7/PdfTkMerger.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\Drupal8\\LibreOfficeConverter' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/Drupal8/LibreOfficeConverter.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\Drupal8\\PdfFromUrl' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/Drupal8/PdfFromUrl.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\Drupal8\\PdfTkMerger' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/Drupal8/PdfTkMerger.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\LibreOfficeConverter' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/LibreOfficeConverter.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfConverter' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfConverter.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfConverterInterface' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfConverterInterface.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfFile' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfFile.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfFromUrl' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfFromUrl.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfFromUrlInterface' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfFromUrlInterface.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfLoginRequiredException' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfLoginRequiredException.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfMergerInterface' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfMergerInterface.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfMetadata' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfMetadata.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfMetadataInterface' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfMetadataInterface.php',
-        'AKlump\\LoftLib\\Component\\Pdf\\PdfTkMetadata' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Pdf/PdfTkMetadata.php',
-        'AKlump\\LoftLib\\Component\\Storage\\FilePath' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Storage/FilePath.php',
-        'AKlump\\LoftLib\\Component\\Storage\\PersistentInterface' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Storage/PersistentInterface.php',
-        'AKlump\\LoftLib\\Component\\Storage\\Records' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Component/Storage/Records.php',
-        'AKlump\\LoftLib\\Drupal\\DrupalBridge' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Drupal/DrupalBridge.php',
-        'AKlump\\LoftLib\\Messenger\\Messenger' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Messenger/Messenger.php',
-        'AKlump\\LoftLib\\Messenger\\MessengerDrupal' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Messenger/MessengerDrupal.php',
-        'AKlump\\LoftLib\\Messenger\\MessengerHtml' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Messenger/MessengerHtml.php',
-        'AKlump\\LoftLib\\Messenger\\MessengerInterface' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Messenger/MessengerInterface.php',
-        'AKlump\\LoftLib\\Messenger\\MessengerShell' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Messenger/MessengerShell.php',
-        'AKlump\\LoftLib\\Testing\\PhpUnitTestCase' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Testing/PhpUnitTestCase.php',
-        'AKlump\\LoftLib\\Utils\\Blocking' => __DIR__ . '/../..' . '/lib/loft_php_lib/dist/src/AKlump/LoftLib/Utils/Blocking.php',
-        'Drupal\\loft_core\\Attribute' => __DIR__ . '/../..' . '/src/Drupal/loft_core/Attribute.php',
-        'Drupal\\loft_core\\Redirect' => __DIR__ . '/../..' . '/src/Drupal/loft_core/Redirect.php',
+    public static $prefixesPsr0 = array (
+        'S' => 
+        array (
+            'Sunra\\PhpSimple\\HtmlDomParser' => 
+            array (
+                0 => __DIR__ . '/..' . '/sunra/php-simple-html-dom-parser/Src',
+            ),
+        ),
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -90,7 +43,7 @@ class ComposerStaticInita53fe36df132dd600a8787a349a9d1f5
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInita53fe36df132dd600a8787a349a9d1f5::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInita53fe36df132dd600a8787a349a9d1f5::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInita53fe36df132dd600a8787a349a9d1f5::$classMap;
+            $loader->prefixesPsr0 = ComposerStaticInita53fe36df132dd600a8787a349a9d1f5::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
