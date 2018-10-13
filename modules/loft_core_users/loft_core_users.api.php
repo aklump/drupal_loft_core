f124a1145a4a5975341815d625c38f1731117e5f
Aaron Klump <sourcecode@intheloftstudios.com>
2018-10-13 16:47:26 -0700

    Refactor before port to 8.x.

--- modules/loft_core_users/loft_core_users.api.php
+++ modules/loft_core_users/loft_core_users.api.php
@@ -5,7 +5,7 @@
  * Return true if the user should NOT be trapped.  The last module to implement
  * this will override all previous.
  */
-function HOOK_loft_core_users_robotrap_access($context) {
+function hook_loft_core_users_robotrap_access($context) {
   return user_is_admin($context['account']);
 }
 
@@ -15,7 +15,7 @@ function HOOK_loft_core_users_robotrap_access($context) {
  * Alter the destination of the path used when trapping users.  Use $context
  * for additional information as needed.
  */
-function HOOK_loft_core_users_robotrap_goto_alter(&$path, &$options, $context) {
+function hook_loft_core_users_robotrap_goto_alter(&$path, &$options, $context) {
   if ($path === 'contact') {
     $options = array('query' => array('cid' => 6));
   }
