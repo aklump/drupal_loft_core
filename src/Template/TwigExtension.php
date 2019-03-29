8a2cce500a4e4b6a2ab3e4381481156a86e5d17e
Aaron Klump <sourcecode@intheloftstudios.com>
2019-03-29 13:59:24 -0700

    Upgrade custom twig functions.

---
+++ src/Template/TwigExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\loft_core\Template;
+
+/**
+ * Provides some Twig functions.
+ */
+class TwigExtension extends \Twig_Extension {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return 'loft_core';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFunctions() {
+    return [
+      new \Twig_SimpleFunction('splitBy', [
+        '\AKlump\LoftLib\Code\Strings',
+        'splitBy',
+      ]),
+      new \Twig_SimpleFunction('loft_core_test_class', 'loft_core_test_class'),
+    ];
+  }
+
+}
