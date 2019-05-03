e838b8f23f91e4e29607b61f6fac332517067724
Aaron Klump <sourcecode@intheloftstudios.com>
2019-05-03 16:46:11 -0700

    Port hook_loft_core_suppress_messages functionality.

--- src/EventSubscriber/ExitSubscriber.php
+++ src/EventSubscriber/ExitSubscriber.php
@@ -18,25 +18,9 @@ class ExitSubscriber implements EventSubscriberInterface {
   }
 
   public function onEvent() {
-
+    // TODO REmove this?
     // If the user has not been unstashed by now, we need to force it before drupal_session_commit() happens.
     loft_core_user_stash();
-
-    // Remove some messages by hook.
-    if (!empty($_SESSION['messages']) && ($masks = \Drupal::moduleHandler()
-        ->invokeAll('loft_core_suppress_messages'))) {
-      foreach (array_keys($masks) as $level) {
-        if (isset($_SESSION['messages'][$level])) {
-          foreach ($_SESSION['messages'][$level] as $key => $message) {
-            foreach ($masks[$level] as $mask) {
-              if (preg_match($mask, $message)) {
-                unset($_SESSION['messages'][$level][$key]);
-              }
-            }
-          }
-        }
-      };
-    }
   }
 
 }
