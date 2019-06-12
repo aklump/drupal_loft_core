15b06d2763dd6704fcabdc262e72cbac45caf250
Aaron Klump <sourcecode@intheloftstudios.com>
2019-06-11 18:17:52 -0700

    set version of phpunit to 5.

--- .web_package/hooks/build/20_run_tests.php
+++ .web_package/hooks/build/20_run_tests.php
@@ -9,5 +9,6 @@ namespace AKlump\WebPackage;
 
 $build
   ->setPhp('/Applications/MAMP/bin/php/php5.6.32/bin/php')
+  ->setPhpUnit('/Users/aklump/opt/phpunit/phpunit-5.phar')
   ->runTests('phpunit.xml')
   ->displayMessages();
