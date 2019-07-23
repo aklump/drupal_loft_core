28ee522c2618ff3c8a23820b8a134c71aa18b5ee
Aaron Klump <sourcecode@intheloftstudios.com>
2019-07-23 13:00:40 -0700

    Version bumped from 8.x-1.0-rc6 to 8.x-1.0-rc7

--- .web_package/hooks/build/00_composer_install.sh
+++ .web_package/hooks/build/00_composer_install.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/Applications/MAMP/bin/php/php5.6.32/bin/php /Users/aklump/bin/composer install --no-dev && composer dumpautoload --optimize && git add composer.lock || build_fail_exception
+/Applications/MAMP/bin/php/php5.6.40/bin/php /Users/aklump/bin/composer install --no-dev && composer dumpautoload --optimize && git add composer.lock || build_fail_exception
