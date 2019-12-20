d223234248556df60db1ade29e9f944e2eb1e67a
Aaron Klump <sourcecode@intheloftstudios.com>
2019-12-20 11:06:47 -0800

    Add entityReverseReference method.

--- src/Loft.php
+++ src/Loft.php
@@ -3,6 +3,7 @@
 namespace Drupal\loft_core;
 
 use AKlump\LoftLib\Code\Cache;
+use Drupal\Core\Entity\EntityInterface;
 
 /**
  * A utility class provided by loft_core.
@@ -72,6 +73,63 @@ class Loft {
     $tree = $menu_tree->transform($tree, $manipulators);
 
     return $menu_tree->build($tree);
+  }
+
+
+  /**
+   * Return an array of entities that point to a known entity.
+   *
+   * Given a known entity, $target_entity, and a reference field name along
+   * with the type of content you want to find that references the known
+   * entity, with optional bundle filter, you may use this method to find those
+   * entities.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $target_entity
+   *   The known entity, which you want to know: What other entities reference
+   *   this one?
+   * @param array $reference_field_names
+   * @param string $referencing_entity_type
+   *   The type of content that you wish to find, which references
+   *   $target_entity.
+   * @param array $referencing_bundle_types
+   *   Optionally, limit the bundles of the content that will be returned, the
+   *   bundles that reference $target_entity.
+   * @param bool $load_entities
+   *   Defaults to true.  Set this to false and an array of ids will be
+   *   returned instead of loading the entities.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface[]|int[]
+   *   An array of reverse reference ids or entities.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public static function entityReverseReference(
+    EntityInterface $target_entity,
+    array $reference_field_names,
+    string $referencing_entity_type,
+    array $referencing_bundle_types = [],
+    bool $load_entities = TRUE
+  ): array {
+    $query = \Drupal::entityQuery($referencing_entity_type);
+    $field_names = $query->orConditionGroup();
+    foreach ($reference_field_names as $reference_field_name) {
+      $field_names->condition($reference_field_name, $target_entity->id());
+    }
+    $query->condition($field_names);
+    if ($referencing_bundle_types) {
+      $query->condition('type', $referencing_bundle_types, 'IN');
+    }
+    $found = $query->execute();
+    if (!$load_entities) {
+      return array_map('intval', array_values($found));
+    }
+
+    $references = \Drupal::entityTypeManager()
+      ->getStorage($referencing_entity_type)
+      ->loadMultiple($found);
+
+    return array_values($references);
   }
 
 }
