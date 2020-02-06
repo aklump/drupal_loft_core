7b5b6fb51aab23fdb4ae3335868f3b65a10b0730
Aaron Klump <sourcecode@intheloftstudios.com>
2020-02-06 13:16:23 -0800

    Add BemTrait

---
+++ src/Utility/BemTrait.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\loft_core\Utility;
+
+/**
+ * Trait BemTrait handles block/element/modifier CSS.
+ *
+ * To use this trait your class must implement the method ::bemBlock, which
+ * returns the base value for BEM concantenation.
+ *
+ * @code
+ *   public function bemBlock(): string {
+ *     return 'user-collections';
+ *   }
+ * @endcode
+ */
+trait BemTrait {
+
+  /**
+   * Return the BEM block string.
+   *
+   * @return string
+   *   The element string.
+   */
+  abstract public function bemBlock(): string;
+
+  /**
+   * Return a BEM element string.
+   *
+   * @param string $suffix
+   *   The suffix to append to the block.
+   *
+   * @return string
+   *   The element string.
+   */
+  public function bemElement(string $suffix): string {
+    list($callback) = loft_core_bem($this->bemBlock());
+
+    return $callback($suffix);
+  }
+
+  /**
+   * Return a BEM modifier string.
+   *
+   * @param string $suffix
+   *   The suffix to append to the block.
+   *
+   * @return string
+   *   The modifier string.
+   */
+  public function bemModifier(string $suffix): string {
+    list(, $callback) = loft_core_bem($this->bemBlock());
+
+    return $callback($suffix);
+  }
+
+}
