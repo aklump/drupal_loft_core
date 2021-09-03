f14cb81975b60bc26b511a6f865681bf2d911b82
Aaron Klump <sourcecode@intheloftstudios.com>
2021-09-02 21:05:32 -0700

    Make hasOutput support MarkupInterface.

--- src/Service/Rendering.php
+++ src/Service/Rendering.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\loft_core\Service;
 
+use Drupal\Component\Render\MarkupInterface;
 use Drupal\Core\Render\Renderer;
 
 /**
@@ -25,21 +26,64 @@ final class Rendering {
   }
 
   /**
-   * Test a render array for actual, content (text, images, etc).
+   * Test a variable to see if it's going to render anything.
    *
-   * @param array $element
-   *   A render element to be checked to see if it outputs anything besides comments or tags.
+   * This can be a render array, a MarkupInterface, or an array of those.  Basically
+   * if you can pass it to Twig in {{ variable }} and want to know if it will show
+   * anything, use this method.
+   *
+   * @param array $variable
+   *   The subject to test.
    *
    * @return bool
    *   TRUE if there is something besides comments and tags.
    */
-  public function hasOutput(array $element): bool {
+  public function hasOutput($variable): bool {
 
     // Isolate the array so it's not mutated by renderPlain.
-    $copy_of_element = array_filter($element);
-    $rendered = trim(strip_tags($this->renderer->renderPlain($copy_of_element)));
+    $copy_of_variable = $variable;
 
-    return boolval($rendered);
+    // Quick reduction of empty content.
+    if (is_array($copy_of_variable)) {
+      $copy_of_variable = array_filter($copy_of_variable);
+    }
+    $has_output = FALSE;
+    $this->hasOutputHelper($copy_of_variable, $has_output);
+
+    return $has_output;
+  }
+
+  private function hasOutputHelper($value, &$output, $key = NULL) {
+
+    if (!is_array($value)) {
+
+      // Ignore properties because they will not render anything.
+      if (is_string($key) && '#' === $key[0]) {
+        return;
+      }
+
+      // These will render by Twig, cast to a string.
+      if ($value instanceof MarkupInterface) {
+        $value = strval($value);
+      }
+      $output = !empty($value);
+    }
+
+    elseif (!is_null($key) && is_array($value) && (is_int($key) || $key === '' || $key[0] !== '#')) {
+      $rendered = trim(strip_tags($this->renderer->renderPlain($value)));
+      $output = boolval($rendered);
+    }
+
+    if ($output) {
+      return;
+    }
+
+    foreach ($value as $key => $item) {
+      $this->hasOutputHelper($item, $output, $key);
+      if ($output) {
+        return;
+      }
+    }
   }
 
 }
