ced18826b0a1a4218520487616c56300d5d16f10
Aaron Klump <sourcecode@intheloftstudios.com>
2024-08-22 11:39:45 -0700

    Replace jquery.once with once.

--- js/clipboard.es6.js
+++ js/clipboard.es6.js
@@ -5,35 +5,35 @@
  */
 import copy from 'copy-to-clipboard';
 
-(function($, Drupal) {
+(function($, Drupal, once) {
   'use strict';
 
   Drupal.behaviors.loftCoreClipboard = {
     attach: function(context) {
-      $('[data-loft-core-clipboard]', context)
-        .once('loft-core-clipboard')
-        .click(function(e) {
-          const $el = $(this);
-          const permalink = $el.data('loft-core-clipboard');
-          const confirm = $el.data('loft-core-clipboard-confirm') || null;
+      $(
+        once('loft-core-clipboard', '[data-loft-core-clipboard]', context)
+      ).click(function(e) {
+        const $el = $(this);
+        const permalink = $el.data('loft-core-clipboard');
+        const confirm = $el.data('loft-core-clipboard-confirm') || null;
 
-          let result = copy(permalink, {
-            debug: true,
-            message: 'Press #{key} to copy',
-          });
-
-          if (result && confirm) {
-            const label = $el.html();
-            const duration =
-              $el.data('loft-core-clipboard-confirm-duration') || 3000;
-            $el.html(confirm);
-            setTimeout(function() {
-              $el.html(label);
-            }, duration);
-          }
-
-          return e.preventDefault();
+        let result = copy(permalink, {
+          debug: true,
+          message: 'Press #{key} to copy',
         });
+
+        if (result && confirm) {
+          const label = $el.html();
+          const duration =
+            $el.data('loft-core-clipboard-confirm-duration') || 3000;
+          $el.html(confirm);
+          setTimeout(function() {
+            $el.html(label);
+          }, duration);
+        }
+
+        return e.preventDefault();
+      });
     },
   };
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
